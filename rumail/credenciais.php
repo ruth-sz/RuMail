4b45d135fa8f775576e79665f4688fd14b2e2910
Ruth S. Z. <ruthsouzazahn@gmail.com>
2025-09-01 20:59:15 -0300

    Delete rumail/credenciais.php

--- rumail/credenciais.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'email' => 'umagaucha95@gmail.com',
-    'senha' => 'jjsc lorp riyv drcy',
-    'email_remetente' => 'umagaucha95@gmail.com',
-    'nome_remetente' => 'Uma Gaucha Remetente',
-    'nome_destinatario' => 'Uma Gaucha Destinatário'
-];
-?>
